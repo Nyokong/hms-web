705fff537b7813874436d66549aa312a649ead69
Nyokong <mikewolfnyokong@gmail.com>
2024-10-17 00:08:35 +0200

    hhhh

--- src/app/feedback/[room]/page.tsx
+++ src/app/feedback/[room]/page.tsx
@@ -3,18 +3,25 @@
 import { useParams } from 'next/navigation';
 import WebSocketFile from '@/components/websockets';
 import MessagesPrint from '@/components/messages';
+import useAuth from '@/app/api/useAuth';
+import Messagesprint from '@/components/messages';
 
 const FeedbackPage = () => {
     const params = useParams();
     const room = params.room;
 
-    return (
-        <div>
-            <h1 className="text-center mt-5">Feedback for Room: {room}</h1>
-            {/* Add your feedback content here */}
-            <WebSocketFile room={room} />
-        </div>
-    );
+    const { user, error } = useAuth();
+
+    if (user) {
+        return (
+            <div>
+                <Messagesprint id={user.id} />
+                <h1 className="text-center mt-5">Feedback for Room: {room}</h1>
+                {/* Add your feedback content here */}
+                <WebSocketFile room={room} />
+            </div>
+        );
+    }
 };
 
 export default FeedbackPage;
